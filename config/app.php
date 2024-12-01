6ea7939d9fdb2e53cbf58fe53c0a47e853cc9698
Viet17032004 <viethmph39977@fpt.edu.vn>
2024-12-01 10:19:17 +0700

    websocket

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        BeyondCode\LaravelWebSockets\WebSocketsServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
     ])->toArray(),
 
