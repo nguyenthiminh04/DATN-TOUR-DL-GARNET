18739bffcd59ba21e9b24b2a2ef7bb9e4da3dba2
lawnos <anhsonw24@gmail.com>
2024-12-09 22:37:07 +0700

    fix status login

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     */
+    public function created(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "updated" event.
+     */
+    public function updated(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     */
+    public function deleted(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     */
+    public function restored(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     */
+    public function forceDeleted(User $user): void
+    {
+        //
+    }
+
+    public function updating(User $user)
+    {
+
+        if ($user->isDirty('status') && $user->status != 1) {
+            Auth::logout();
+        }
+    }
+}
